29f3c8b6e53f3bf1180809bd6026573184f5bd5d
jojunhyeong <jojh0323@naver.com>
2025-08-25 14:45:03 +0900

    refactor: TransactionCard와 TransactionList 이름 변경 및 components/transactions로 이동

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -1,12 +1,12 @@
 import MonthNavigator from '@/components/dashboard/MonthNavigator'
 import StatCard from '@/components/dashboard/StatCard'
-import TransactionCard from '@/components/dashboard/TransactionCard'
+import TransactionList from '@/components/transactions/TransactionList'
 
 export default function DashBoard() {
   const stats = [
-    { id: 1, title: '이번 달 수입', amount: '3,000,000', rate: '+12.5%' ,type:'income' },
-    { id: 2, title: '이번 달 지출', amount: '1,850,000', rate: '+8.3%', type:'expense' },
-    { id: 3, title: '순수입', amount: '1,150,000', rate: '+15.2%', type:'net'},
+    { id: 1, title: '이번 달 수입', amount: '3,000,000', rate: '+12.5%', type: 'income' },
+    { id: 2, title: '이번 달 지출', amount: '1,850,000', rate: '+8.3%', type: 'expense' },
+    { id: 3, title: '순수입', amount: '1,150,000', rate: '+15.2%', type: 'net' },
   ]
 
   return (
@@ -14,10 +14,16 @@ export default function DashBoard() {
       <MonthNavigator />
       <div className="flex gap-10 mt-8">
         {stats.map((stat) => (
-          <StatCard key={stat.id} title={stat.title} amount={stat.amount} rate={stat.rate} type={stat.type} />
+          <StatCard
+            key={stat.id}
+            title={stat.title}
+            amount={stat.amount}
+            rate={stat.rate}
+            type={stat.type}
+          />
         ))}
       </div>
-      <TransactionCard/>
+      <TransactionList />
     </div>
   )
 }
