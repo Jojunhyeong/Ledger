72eca65cfec88a313557c8d663c630b4f81b0025
jojunhyeong <jojh0323@naver.com>
2025-09-03 16:13:56 +0900

    chore: 의존성 업데이트

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin'
-const dirname =
-  typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url))
+
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 const __filename = fileURLToPath(import.meta.url)
@@ -25,11 +23,8 @@ export default defineConfig({
       {
         extends: true,
         plugins: [
-          // The plugin will run tests for the stories defined in your Storybook config
-          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-          storybookTest({
-            configDir: path.join(dirname, '.storybook'),
-          }),
+          
+          
         ],
         test: {
           name: 'storybook',
@@ -43,7 +38,7 @@ export default defineConfig({
               },
             ],
           },
-          setupFiles: ['.storybook/vitest.setup.js'],
+        
         },
       },
     ],
