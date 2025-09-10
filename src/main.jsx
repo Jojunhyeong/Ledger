dffd5481155844eaa4c7fac14aaa7dab8fd29fa9
jojunhyeong <jojh0323@naver.com>
2025-09-10 15:31:39 +0900

    feat: 전역 인증 스토어(useAuthStore) 생성 및 세션/유저 상태 관리

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import './index.css'
 import App from './App.jsx'
 import AppRoutes from './router'
 import { BrowserRouter } from 'react-router-dom'
+import { useAuthStore } from './store/useAuthStore'
+
+useAuthStore.getState().init();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
